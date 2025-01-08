c9d0817a06267cfd650d7736715d375892b316af
Felix St-Denis <felix.st-denis@umontreal.ca>
2025-01-08 14:08:01 +0900

    First, added Benjamin code

---
+++ ThinElasticPlate.py
@@ -0,0 +1,28 @@
+"""
+The goal of this code is to write the deformation of a 
+thin elastic plate as described by Tremblay et al. 2025
+
+Wrote by Félix St-Denis. 
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def wave_height():
+    
+    
+    
+    return
+
+
+def strain():
+    
+    
+    return 
+
+
+def moment_inertia():
+    
+    
+    return
